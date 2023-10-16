d0b727e7af2b4140e30d2878d3a44bb7403b6cb9
ArinoMichi <cutesyprogrammer@gmail.com>
2023-10-16 10:49:26 +0200

    correction

--- src/index.js
+++ src/index.js
@@ -3,13 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
-import Imagen from './components/Imagen';
-import SumarNumeros from './components/SumarNumeros';
+import SumarNumeros from './components/SumarNumeros/SumarNumeros';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Imagen />
     <SumarNumeros/>
   </React.StrictMode>
 );
